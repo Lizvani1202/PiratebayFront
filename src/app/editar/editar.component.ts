63a52018cdf96b93e93501d8af8614df25952fad
Liz Vania <lizveraduran@gmail.com>
2020-07-02 13:33:16 -0400

    Cambio en editar

--- src/app/editar/editar.component.ts
+++ src/app/editar/editar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { OrdenServiceService } from '../service/orden-service.service';
 import { Router } from '@angular/router';
 import { Orden } from '../models/orden';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-editar',
@@ -31,15 +32,24 @@ export class EditarComponent implements OnInit {
     productoTipo:null,
     tx_date:null
   };
-  Update:any={
+  Update1:any={
     numeroOrden:null,
     fechaPreparado: null,
     estado:null
 }
+Update2:any={
+  numeroOrden:null,
+  fechaDespachado: null,
+  estado:null
+}
+Update3:any={
+  numeroOrden:null,
+  fechaEntregado: null,
+  estado:null
+}
   ObtenerDatos(){
     this.Ordenservice.OrdgenGetbyId(localStorage.getItem("id")).subscribe(data=>{
      this.Orden=data;
-     this.Orden.estado=3;
     },error=>{
 
     });
@@ -50,16 +60,51 @@ export class EditarComponent implements OnInit {
   volver(){
     this.router.navigate(['/']);
   }
-  actualizar(id){
-    this.Update.numeroOrden=id;
-  }
   Preparado(id){
+    let date = new Date()
+    let day = date.getDate()
+    let month = date.getMonth() + 1
+    let year = date.getFullYear()
+    let fecha=day+"/"+month+"/"+year;
+    this.Update1.numeroOrden=this.Orden.numeroOrden;
+    this.Update1.fechaPreparado=fecha;
+    this.Update1.estado=1;
+    this.Ordenservice.OrdenPut(this.Update1).subscribe(data=>{
+      console.log(data);
+    },error=>{
 
+    });
   }
   Despachado(id){
+    let date = new Date()
+    let day = date.getDate()
+    let month = date.getMonth() + 1
+    let year = date.getFullYear()
+    let fecha=day+"/"+month+"/"+year;
+    this.Update2.numeroOrden=this.Orden.numeroOrden;
+    this.Update2.fechaDespachado=fecha;
+    this.Update2.estado=2;
+    this.Ordenservice.OrdenPut(this.Update2).subscribe(data=>{
+      console.log(data);
+    },error=>{
+
+    });
 
   }
   Entregado(id){
+    let date = new Date()
+    let day = date.getDate()
+    let month = date.getMonth() + 1
+    let year = date.getFullYear()
+    let fecha=day+"/"+month+"/"+year;
+    this.Update3.numeroOrden=this.Orden.numeroOrden;
+    this.Update3.fechaEntregado=fecha;
+    this.Update3.estado=3;
+    this.Ordenservice.OrdenPut(this.Update3).subscribe(data=>{
+      console.log(data);
+    },error=>{
+
+    });
 
   }
 
